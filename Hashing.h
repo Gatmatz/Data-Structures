cba89b0ff2c8b2185c9e7d389fdcd0e61f3343c3
Giorgos Atmatzidis <gatmatzidi@gmail.com>
2021-06-01 16:26:25 +0300

    Update Hashing.h

--- Hashing.h
+++ Hashing.h
@@ -11,15 +11,16 @@ private:
     int size; //Size of words
     float load_factor; //Load factor size/capacity
 public:
-    int hashkey(string,int); //Linear Hashing
+    unsigned long hashkey(const char*,int); //Linear Hashing with djb2 algorithm
     //Constructor
     Hash(); //Initial size of HashTable:1000
     //Other Functions
-    void insert(string&); //insert word in hashtable
+    void insert(string); //insert word in hashtable
     int HashSearch(string); //returns the position of a word
     void checkRehash(); //Checks if load_factor>0.5
     int getSize(); //returns the number of words in HashTable
     int getFreq(int); //Returns the frequency of a word
+    string operator[](int); //Returns word in that position
     //Friendly operators
     friend ostream& operator<<(ostream&,Hash&);
 };
