b81e83679182f66f305ce2d5b4e4ef65dbbb4ae5
Giorgos Atmatzidis <gatmatzidi@gmail.com>
2021-06-08 01:44:45 +0300

    Update BinarySearchTree.h

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -16,8 +16,8 @@ class BinarySearchTree
 {
 private:
     node *root; //Root Pointer
-    void insert(string,node*); //Inserts a new node to its right place
-    node* search(string,node*);//Searches for a node with that string
+    void insert(string&,node*); //Inserts a new node to its right place
+    node* search(string&,node*);//Searches for a node with that string
     void PrintPreorder(node*,ostream&); //Prints in preorder traversal
     void PrintInorder(node *,ostream&); //Prints in inorder traversal
     void PrintPostorder(node *,ostream&); //Prints in postorder traversal
@@ -29,15 +29,17 @@ private:
 public:
     //Constructors
     BinarySearchTree(); //Initialize the root ptr to NULL
+    //Destructors
+    ~BinarySearchTree();
     //Other functions/ User-friendly versions
-    void insert(string); //Create a new node with this string
-    void DeleteNode(string); //Delete Node from tree
+    void insert(string&); //Create a new node with this string
+    void DeleteNode(string&); //Delete Node from tree
     void DeleteTree(); //Delete entire tree
-    node* search(string);//Returns the node with this string
+    node* search(string&);//Returns the node with this string
     void PrintPreorder(ostream&); //Prints in preorder traversal/accepts an output stream
     void PrintInorder(ostream&); //Prints in inorder traversal/accepts an output stream
     void PrintPostorder(ostream&); //Prints in postorder traversal/accepts an output stream
-    int getCount(string); //Returns the frequency of a string
+    int getCount(string&); //Returns the frequency of a string
     node *findMin(); //Find node with minimum word(ASCII Value)
     node *findMax(); //Find node with maximum word(ASCII Value)
     int getFreq(node*); //Returns the frequency of a node
