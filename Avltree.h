170de947c415eb8fd8c6d8a66c0f096630629eef
Giorgos Atmatzidis <gatmatzidi@gmail.com>
2021-06-18 14:23:32 +0300

    Update Avltree.h

--- Avltree.h
+++ Avltree.h
@@ -2,18 +2,9 @@
 #define avltree_h
 #include <iostream>
 #include <string>
-#include "struct_value.h"
+#include "moreTools.h"
 
 using namespace std;
-
-struct node
-{
-    value word; //word and its frequency
-    node* left; //left child
-    node* right; //right child
-    node* parent; //parent
-    //int height;// height of the tree
-};
 
 class Avltree
 {
@@ -22,24 +13,24 @@ private:
 
     node* insert(string &, node*); //inserts new node
     node* search(string &, node*); //searches for node with string
-    void deleteNode (node*); //deletes node
+    void deleteNode (node*); //Sets the frequency of the word to zero
     void deleteTree (node*); //deletes entire tree
 
-    int getheight(node*);
+    int getHeight(node*);
 
     void preorder (node*, ostream&); //prints tree in preorder
     void inorder (node*, ostream&); //prints tree in inorder
     void postorder (node*, ostream&); //prints tree in postorder
 
-    node* findMin(node*); //finds node with minimum word alphabeticallly
-    node* findMax(node*); //finds node with maximum word alphabeticallly
+    node* findMin(node*); //finds node with minimum word alphabetically
+    node* findMax(node*); //finds node with maximum word alphabetically
     int getBalance(node*); //returns balance of node
-    
+
 public:
     Avltree(); //empty constructor
 
-    int getheight(); //returns the hight of the tree
-    
+    int getHeight(); //returns the hight of the tree
+
     void insert(string &); //creates new node including string
     node* search(string &); //searches for node with string
     void deleteNode (string &); //deletes node
@@ -49,16 +40,14 @@ public:
     void inorder (ostream&); //prints tree in inorder
     void postorder (ostream&); //prints tree in postorder
 
-    node* findMin(); //finds node with minimum word alphabeticallly
-    node* findMax(); //finds node with maximum word alphabeticallly
+    node* findMin(); //finds node with minimum word alphabetically
+    node* findMax(); //finds node with maximum word alphabetically
 
     int getFreq(string &); //returns frequency of string
     int getNodeFreq(node*); //returns frequency of node
 
     node* leftRotate(node*); //left rotates subtree
     node* rightRotate(node*); //right rotates subtree
-    node* leftrightRotate(node*); //left right
-    node* rightleftRotate(node*); //right left
 
     int getBalance(); //returns balance of node
 };
